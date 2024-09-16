2f8f10f727ef524ca18fe75d8feaa2a87718f89a
Salem <salo0002@stud.kea.dk>
2024-09-16 10:58:01 +0200

    mm

---
+++ src/main/java/com/example/turistguidedel2/TuristguideDel2Application.java
@@ -0,0 +1,13 @@
+package com.example.turistguidedel2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristguideDel2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuristguideDel2Application.class, args);
+    }
+
+}
